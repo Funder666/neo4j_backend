462272f7e6dffbd9eb79e59987c8a1aa4084a48f
Restart <841610078@qq.com>
2025-11-09 10:51:43 +0800

    语料库查询更新

--- api/corpus/search_query.py
+++ api/corpus/search_query.py
@@ -7,14 +7,14 @@ content = input("请输入要查询的内容：")
 
 
 def query_post(query):
-    url = "https://corpus.chineseplus.net/api/v1/search/edu"
+    url = "https://corpus.chineseplus.net/corpus/api/v1/search/0/corpus"
     data = {"query": query}
     headers = {"Content-Type": "application/json; charset=UTF-8"}
     try:
         response = requests.post(url, json=data, headers=headers)
         print("状态码:", response.status_code)
-        print("检索类型:", response.json()["data"]["Type"])
-        print("检索返回数量:", response.json()["data"]["total"])
+        # print("检索类型:", response.json()["data"]["Type"])
+        # print("检索返回数量:", response.json()["data"]["total"])
         print("返回数据:", response.json()["data"])
     except requests.exceptions.RequestException as e:
         print("请求错误:", e)
